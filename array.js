1dcb02f5a5dde12a9e260d546b29ce0576de0e26
juuclue <144699439+juuclue@users.noreply.github.com>
2024-01-30 10:27:41 -0500

    e

---
+++ array.js
@@ -0,0 +1,66 @@
+const dishes = [
+    {
+      name: "Yakisoba (焼きそば)",
+      category: "Japanese",
+      image: "/HIYASHI.jpg",
+    },
+    {
+        name: "Yakisoba (焼きそば)",
+        category: "Japanese",
+        image: "/HIYASHI.jpg",
+    },
+    {
+        name: "Yakisoba (焼きそば)",
+        category: "Japanese",
+        image: "/HIYASHI.jpg",
+    },
+    {
+      name: "Harry Potter and the Deathly Hallows",
+      authorFirst: "JK",
+      authorLast: "Rowling",
+      publishDate: 1997,
+      genre: ["fantasy", , "fiction"],
+    },
+    {
+      name: "Ulysses",
+      authorFirst: "James",
+      authorLast: "Joyce",
+      publishDate: 1918,
+      genre: ["historical", "non-fiction"],
+    },
+    {
+      name: "War and Peace",
+      authorFirst: "Leo",
+      authorLast: "Tolstoy",
+      publishDate: 1867,
+      genre: ["historical", "fiction"],
+    },
+    {
+      name: "Pride and Prejudice",
+      authorFirst: "Jane",
+      authorLast: "Austen",
+      publishDate: 1813,
+      genre: ["fiction"],
+    },
+    {
+      name: "The Catcher in the Rye",
+      authorFirst: "JD",
+      authorLast: "Salinger",
+      publishDate: 1951,
+      genre: ["fiction"],
+    },
+    {
+      name: "Great Expectations",
+      authorFirst: "Charles",
+      authorLast: "Dickens",
+      publishDate: 1861,
+      genre: ["historical", "fiction"],
+    },
+    {
+      name: "Little Women",
+      authorFirst: "Lousia May",
+      authorLast: "Alcott",
+      publishDate: 1868,
+      genre: ["womens", "historical", "fiction"],
+    },
+  ];
