889aea9d4d337f9262bd51f378f42d18dd6738ca
juuclue <144699439+juuclue@users.noreply.github.com>
2024-02-01 10:12:30 -0500

    e

--- array.js
+++ array.js
@@ -1,52 +1,128 @@
 const dishes = [
     {
       name: "Yakisoba (焼きそば)",
-      category: "Japanese",
+      category: "Japan",
       image: "/YAKISOBA.jpg",
     },
     {
         name: "冷やし中華 (hiyashi chuka)",
-        category: "Japanese",
+        category: "Japan",
         image: "/HIYASHI.jpg",
     },
     {
         name: "かけうどん (Kake Udon)",
-        category: "Japanese",
+        category: "Japan",
         image: "/KAKEUDON.jpg",
     },
     {
       name: "醤油ラメーン (Shoyu ramen)",
-      category: "Japanese",
+      category: "Japan",
       image: "/SHOYU.jpg",
     },
     {
       name: "味噌ラーメン (Miso ramen)",
-      category: "Japanese",
+      category: "Japan",
       image: "/MISO.jpg",
     },
     {
       name: "豚骨/とんこつ (Tonkotsu ramen)",
-      category: "Japanese",
+      category: "Japan",
       image: "/TONKOTSU.jpg",
     },
     {
-      name: "かけうどん (Kake Udon)",
-      category: "Japanese",
-      image: "/KAKEUDON.jpg",
+      name: "Dao Xiao Mian",
+      category: "China",
+      image: "/DAO.jpg",
     },
     {
-      name: "かけうどん (Kake Udon)",
-      category: "Japanese",
-      image: "/KAKEUDON.jpg",
+      name: "Fen Si",
+      category: "China",
+      image: "/FENSI.jpg",
     },
     {
-      name: "かけうどん (Kake Udon)",
-      category: "Japanese",
-      image: "/KAKEUDON.jpg",
+      name: "Chao Mein",
+      category: "China",
+      image: "/CHOWMEIN.jpg",
     },
     {
-      name: "かけうどん (Kake Udon)",
-      category: "Japanese",
-      image: "/KAKEUDON.jpg",
+      name: "Peanut Butter noodles",
+      category: "China",
+      image: "/PEANUT.jpg",
     },
+    {
+      name: "gochujang noodles",
+      category: "Korea",
+      image: "/GOCHU.jpg",
+    },
+    {
+      name: "Jajangmyeon",
+      category: "Korea",
+      image: "/JAJA.jpg",
+    },
+    {
+      name: "Japchae",
+      category: "Korea",
+      image: "/JAPCHAE.jpg",
+    },
+    {
+      name: "Pho",
+      category: "Vietnam",
+      image: "/PHO.jpg",
+    },
+    {
+      name: "Hu Tieu",
+      category: "Vietnam",
+      image: "/HUTIEU.jpg",
+    },
+    {
+      name: "Mi Quang",
+      category: "Vietnam",
+      image: "/MIQUANG.jpg",
+    },
+    {
+      name: "Bun Rieu",
+      category: "Vietnam",
+      image: "/BUNRIEU.jpg",
+    },
+    {
+      name: "Mie Goreng",
+      category: "Indonesia",
+      image: "/MIEGORENG.jpg",
+    },
+    {
+      name: "Pad Thai",
+      category: "Thailand",
+      image: "/PADTHAI.jpg",
+    },
+    {
+      name: "Hakka noodles",
+      category: "India",
+      image: "/HAKKA.jpg",
+    },
+    {
+      name: "Singapore-style noodles",
+      category: "Singapore",
+      image: "/SINGA.jpg",
+    },
+    {
+      name: "Char Kway Teow",
+      category: "Malaysia",
+      image: "/CHAR.jpg",
+    },
+    {
+      name: "Thukpa",
+      category: "Nepali",
+      image: "/Thukpa.jpg",
+    },
+    {
+      name: "Sherpa Soup Noodles",
+      category: "Nepali",
+      image: "/SHERPA.jpg",
+    },
+    {
+      name: "Gundruk",
+      category: "Nepali",
+      image: "/GUNDRUK.jpg",
+    },
+    
   ];
---
+++ main.js
@@ -0,0 +1,16 @@
+const DOMSelectors = {
+
+    China: document.querySelector(".China"),
+    Korea: document.querySelector(".Korea"),
+    India: document.querySelector(".India"),
+    Indonesia: document.querySelector(".Indonesia"),
+    Thailand: document.querySelector(".Thailand"),
+    Vietnam: document.querySelector(".Vietnam"),
+    Japan: document.querySelector(".Japan"),
+    Nepali: document.querySelector(".Nepali"),
+    Singapore: document.querySelector(".Singapore"),
+    Malaysia: document.querySelector(".Malaysia"),
+    submit: document.getElementById("#submit"),
+
+};
+
